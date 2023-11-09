c844a9eb60d3777fd9e902ac49f5f62616b9c240
abhiram1117 <143382378+abhiram1117@users.noreply.github.com>
2023-11-09 13:45:05 +0530

    Add files via upload

---
+++ StudentRepository.cs
@@ -0,0 +1,43 @@
+﻿using SchoolManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SchoolManagement.Data
+{
+    public class StudentRepository
+    {
+        private List<Student> students = new List<Student>();
+        private List<string> subjects = new List<string> { "Telugu", "Hindi", "English", "Math", "Science", "Social" };
+
+        public List<Student> GetStudents()
+        {
+            return students;
+        }
+
+        public Student GetStudentByRollNumber(string rollNumber)
+        {
+            return students.FirstOrDefault(s => s.RollNumber == rollNumber);
+        }
+
+        public void AddStudent(Student student)
+        {
+            students.Add(student);
+        }
+
+        public void AddMarks(Student student, string subject, double marks)
+        {
+            student.Marks[subject] = marks;
+        }
+
+        public double GetMarks(Student student, string subject)
+        {
+            return student.Marks.ContainsKey(subject) ? student.Marks[subject] : 0.0;
+        }
+
+        public List<string> GetSubjects()
+        {
+            return subjects;
+        }
+    }
+}
---
+++ StudentService.cs
@@ -0,0 +1,63 @@
+﻿using SchoolManagement.Data;
+using SchoolManagement.Models;
+
+
+namespace SchoolManagement.Business
+{
+    public class StudentManagement
+    {
+        private string schoolName;
+        private StudentRepository studentRepository;
+
+        public StudentManagement(string schoolName)
+        {
+            this.schoolName = schoolName;
+            studentRepository = new StudentRepository();
+        }
+
+        public void AddStudent(string rollNumberInput, string studentName)
+        {
+            var student = new Student(rollNumberInput, studentName);
+            studentRepository.AddStudent(student);
+        }
+
+        public void AddMarks(string rollNumber, string subject, double marks)
+        {
+            var student = studentRepository.GetStudentByRollNumber(rollNumber);
+            if (student != null)
+            {
+                studentRepository.AddMarks(student, subject, marks);
+            }
+        }
+
+        public Student GetStudentByRollNumber(string rollNumber)
+        {
+            return studentRepository.GetStudentByRollNumber(rollNumber);
+        }
+
+        public double CalculateTotalMarks(Student student)
+        {
+            double total = 0.0;
+            foreach (var subject in studentRepository.GetSubjects())
+            {
+                total += studentRepository.GetMarks(student, subject);
+            }
+            return total;
+        }
+
+
+        public double CalculatePercentage(Student student)
+        {
+            double totalMarks = CalculateTotalMarks(student);
+            return (totalMarks / (studentRepository.GetSubjects().Count * 100))*100;
+        }
+        public List<string> GetSubjects()
+        {
+            return studentRepository.GetSubjects();
+        }
+        public double GetMarks(Student student ,  string subject)
+        {
+            return studentRepository.GetMarks(student, subject);
+        }
+    }
+}
