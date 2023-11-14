d5cc41a7d480dec37d093405d27ead1ee9900c23
abhiram1117 <abhiram1117@gmail.com>
2023-11-14 15:59:52 +0530

    Update StudentService.cs

--- StudentManagement.Services/StudentService.cs
+++ StudentManagement.Services/StudentService.cs
@@ -1,5 +1,7 @@
 ﻿using SchoolManagement.Data;
 using SchoolManagement.Models;
+using System.Collections.Generic;
+
 
 
 namespace SchoolManagement.Business
@@ -10,7 +12,11 @@ namespace SchoolManagement.Business
         private List<Student> students = new List<Student>();
         private StudentRepository studentRepository;
         private MarksRepository marksRepository;
+<<<<<<< Updated upstream:StudentManagement.Services/StudentService.cs
         private int studentIdCounter = 1;
+=======
+        public int StudentIdCounter = 1;
+>>>>>>> Stashed changes:Bussiness Layer/StudentService.cs
 
         public StudentService(string schoolName)
         {
@@ -25,8 +31,13 @@ namespace SchoolManagement.Business
 
         public void AddStudent(string rollNumberInput, string studentName)
         {
+<<<<<<< Updated upstream:StudentManagement.Services/StudentService.cs
             int studentId = GenerateUniqueId();
             Student student = new Student(studentId,rollNumberInput, studentName);
+=======
+            int id = GenerateUniqueId();
+            Student student = new Student(id, rollNumberInput, studentName);
+>>>>>>> Stashed changes:Bussiness Layer/StudentService.cs
             studentRepository.AddStudent(student);
         }
 
@@ -70,9 +81,15 @@ namespace SchoolManagement.Business
         {
             return marksRepository.GetMarks(student, subject);
         }
+<<<<<<< Updated upstream:StudentManagement.Services/StudentService.cs
         private int GenerateUniqueId()
         {
             return studentIdCounter++;
+=======
+        public int GenerateUniqueId()
+        {
+            return StudentIdCounter++;
+>>>>>>> Stashed changes:Bussiness Layer/StudentService.cs
         }
     }
 }
